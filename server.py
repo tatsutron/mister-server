d105da42ce8edacfc9515c56b694843e685b4946
tatsutron <51144928+tatsutron@users.noreply.github.com>
2022-04-13 09:47:01 -0500

    Update paths

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from bottle import put, request, route, run
 from xml.etree.ElementTree import Element, ElementTree, SubElement
 
 
+ROOT = "/media/fat/tatsutron/mister-server"
+
+
 @route("/scan/<extensions>/<path:path>")
 def scan(extensions, path):
     entries = []
@@ -20,17 +23,17 @@ def scan(extensions, path):
 @route("/hash/<header_size>/<path:path>")
 def hash(header_size, path):
     return subprocess.run(
-        ["/media/fat/tatsutron/hash", path, "{}".format(header_size)],
+        [f"{ROOT}/hash", path, "{}".format(header_size)],
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
     ).stdout
 
 
-# TODO Remove afte MGL migration
+# TODO Remove post MGL migration
 @route("/load/<command>/<path:path>")
 def load(command, path):
     return subprocess.run(
-        ["/media/fat/tatsutron/mbc", "load_rom", command, path],
+        [f"${ROOT}/mbc", "load_rom", command, path],
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
     ).stdout
@@ -46,10 +49,10 @@ def play():
     file.set("index", request.json["index"])
     file.set("path", request.json["path"])
     file.set("type", request.json["type"])
-    with open("/media/fat/tatsutron/temp.mgl", "w") as file:
+    with open(f"{ROOT}/temp.mgl", "w") as file:
         ElementTree(root).write(file, encoding='unicode')
     with open("/dev/MiSTer_cmd", "w") as file:
-        file.write("load_core /media/fat/tatsutron/temp.mgl")
+        file.write(f"load_core ${ROOT}/temp.mgl")
 
 
 run(host='0.0.0.0', port=8080, debug=True)
